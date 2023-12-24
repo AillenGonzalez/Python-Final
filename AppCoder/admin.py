814e65dbfc0660bb1da0816a0009404ce12f7f47
AillenGonzalez <ailleng@hotmail.com>
2023-12-24 19:51:09 -0300

    First commit

---
+++ AppCoder/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.Profesor)
+admin.site.register(models.Curso)
+admin.site
+admin.site.register(models.Post)
+admin.site.register(models.Comentario)
+admin.site.register(models.Perfil)
+
